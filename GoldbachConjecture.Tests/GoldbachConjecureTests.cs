16adb5bd1e0b139d29fa846aed11461bc7804078
lorcaraul <raul_lorca@hotmail.com>
2015-08-18 11:33:16 +0200

    retorna el resultado harcodeado

---
+++ GoldbachConjecture.Tests/GoldbachConjecureTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GoldbachConjecture.Tests
+{
+    [TestClass]
+    public class GoldbachConjecureTests
+    {
+        private GoldbachConjecture goldbachConjecture ;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            goldbachConjecture = new GoldbachConjecture();
+        }     
+        
+        [TestMethod]
+        public void Test_5()
+        {
+            var result = goldbachConjecture.Result(5);
+            Assert.AreEqual(2, result.Item1);
+            Assert.AreEqual(3, result.Item2);
+
+        }
+    }
+}
---
+++ GoldbachConjecture/GoldbachConjecture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldbachConjecture
+{
+    public class GoldbachConjecture
+    {
+
+        public Tuple<int,int> Result(int number)
+        {
+            return new Tuple<int, int>(2, 3);
+        }
+
+    }
+}
